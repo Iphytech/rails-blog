c02654e2c81ab27fcbe57eeb382c7d6c246970d3
Ifunanya Ikemma <ifavour902@gmail.com>
2019-06-14 17:02:09 +0100

    Adding logics for comment controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_193321) do
+ActiveRecord::Schema.define(version: 2019_06_14_142016) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
